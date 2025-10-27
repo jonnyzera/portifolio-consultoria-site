23e75d781d1df6882266ed3932d046e6297748d8
João Victor <joaovictor8600@yahoo.com.br>
2025-10-27 16:52:10 -0300

    feat(front-end): Setup inicial do portfólio, UI/UX gamificado com Bootstrap e CSS

---
+++ portfolio-consultoria/src/main/java/com/consultoria/dev/portfolio/portfolio_consultoria/PortfolioConsultoriaApplication.java
@@ -0,0 +1,13 @@
+package com.consultoria.dev.portfolio.portfolio_consultoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioConsultoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioConsultoriaApplication.class, args);
+	}
+
+}
---
+++ portfolio-consultoria/src/test/java/com/consultoria/dev/portfolio/portfolio_consultoria/PortfolioConsultoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consultoria.dev.portfolio.portfolio_consultoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioConsultoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
