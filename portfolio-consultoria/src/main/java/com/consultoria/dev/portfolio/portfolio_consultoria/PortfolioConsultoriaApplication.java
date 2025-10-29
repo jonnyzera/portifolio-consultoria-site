7f96a41366e626d49759a8236b62207f43ce6338
Your Name <you@example.com>
2025-10-29 17:24:41 -0300

    feat: Remoção do JPA e Banco de Dados

--- portfolio-consultoria/src/main/java/com/consultoria/dev/portfolio/portfolio_consultoria/PortfolioConsultoriaApplication.java
+++ portfolio-consultoria/src/main/java/com/consultoria/dev/portfolio/portfolio_consultoria/PortfolioConsultoriaApplication.java
@@ -2,13 +2,8 @@ package com.consultoria.dev.portfolio.portfolio_consultoria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {
-	DataSourceAutoConfiguration.class,
-	JpaRepositoriesAutoConfiguration.class
-})
+@SpringBootApplication
 public class PortfolioConsultoriaApplication {
 
 	public static void main(String[] args) {
