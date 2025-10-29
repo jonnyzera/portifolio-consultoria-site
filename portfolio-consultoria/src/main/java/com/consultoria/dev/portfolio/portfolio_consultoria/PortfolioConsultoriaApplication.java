3353661aea65b9780f65f34a795b66982be8499f
Your Name <you@example.com>
2025-10-29 16:27:11 -0300

    build(config): Desabilita autoconfiguração de banco de dados na inicialização

--- portfolio-consultoria/src/main/java/com/consultoria/dev/portfolio/portfolio_consultoria/PortfolioConsultoriaApplication.java
+++ portfolio-consultoria/src/main/java/com/consultoria/dev/portfolio/portfolio_consultoria/PortfolioConsultoriaApplication.java
@@ -2,8 +2,13 @@ package com.consultoria.dev.portfolio.portfolio_consultoria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+	DataSourceAutoConfiguration.class,
+	JpaRepositoriesAutoConfiguration.class
+})
 public class PortfolioConsultoriaApplication {
 
 	public static void main(String[] args) {
